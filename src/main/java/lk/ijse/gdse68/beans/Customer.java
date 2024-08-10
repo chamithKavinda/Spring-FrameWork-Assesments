4f5fc0e191491b05fcd65a91bcbe325bfe98edde
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-10 12:28:17 +0530

    added missing config

--- src/main/java/lk/ijse/gdse68/beans/Customer.java
+++ src/main/java/lk/ijse/gdse68/beans/Customer.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse68.beans;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Customer {
     public Customer(){
         System.out.println("Hello Customer");
--- src/main/java/lk/ijse/gdse68/beans/Item.java
+++ src/main/java/lk/ijse/gdse68/beans/Item.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse68.beans;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Item {
     public Item(){
         System.out.println("Hello Item");
