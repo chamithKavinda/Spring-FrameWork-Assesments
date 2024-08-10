760f9aa9d46f0707341af568099fa76c08b0010f
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-10 12:44:26 +0530

    add annotation dependancies , implement TestBeans

--- src/main/java/lk/ijse/gdse68/beans/TestBeans.java
+++ src/main/java/lk/ijse/gdse68/beans/TestBeans.java
@@ -1,10 +1,16 @@
 package lk.ijse.gdse68.beans;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+//You can use this to give a name to the bean
+//If you did not give a name to the bean it will take the class name as the bean name
+//Eg: TestBeans -> testBeans
+@Component("TestBeans")
 public class TestBeans implements BeanNameAware, BeanFactoryAware,
         ApplicationContextAware, InitializingBean, DisposableBean {
     @Override
@@ -17,9 +23,19 @@ public class TestBeans implements BeanNameAware, BeanFactoryAware,
         System.out.println("Bean factory is " + beanFactory );
     }
 
+    //Use this method to do something before destroy method
+    //To clean up resources (eg: closing a database connection)
     @Override
     public void destroy() throws Exception {
         System.out.println("Hello World");
+    }
+
+    //This is not a part of spring framework its additional
+    //if you want to do any custom destroy method or do something before destroy method
+    //You can use this annotation
+    @PreDestroy
+    public void customDestroy(){
+        System.out.println("Custom destroy method");
     }
 
     @Override
