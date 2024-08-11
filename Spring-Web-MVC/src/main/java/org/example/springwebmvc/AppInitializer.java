7724df44a7c2f6c4bf4fa2ac07241539f6b75d8e
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-11 13:18:34 +0530

    Init project with create dispatcher servlet and add config classes

--- Spring-Web-MVC/src/main/java/org/example/springwebmvc/AppInitializer.java
+++ Spring-Web-MVC/src/main/java/org/example/springwebmvc/AppInitializer.java
@@ -1,5 +1,24 @@
 package org.example.springwebmvc;
 
-public class AppInitializer {
+import org.example.springwebmvc.config.WebAppConfig;
+import org.example.springwebmvc.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 
 }
