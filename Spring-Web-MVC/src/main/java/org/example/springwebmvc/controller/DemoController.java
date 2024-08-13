72bac405fe5d448e73ccd357623de500d8b80c42
chamithKavinda <chamith13kavinda@gmail.com>
2024-08-13 20:37:57 +0530

    Test mapping with path variable

--- Spring-Web-MVC/src/main/java/org/example/springwebmvc/controller/DemoController.java
+++ Spring-Web-MVC/src/main/java/org/example/springwebmvc/controller/DemoController.java
@@ -1,14 +1,24 @@
 package org.example.springwebmvc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/demo")
 @RestController
 public class DemoController {
-    @GetMapping(value = "/health")
-    public String healthCheck(){
-        return "DemoController run perfectly";
+    @GetMapping(value = "/pattern/{name}")
+    public String pathVariable(@PathVariable ("name") String name) {
+        return "DemoController run perfectly with :"+name;
+    }
+    @GetMapping(value = "/patternRegex/{id:C\\d{3}}")
+    public String pathvariableWithRegex(@PathVariable("id") String id) {
+        return "DemoController run perfectly with :"+id;
+    }
+
+    @GetMapping
+    public String otherTest(){
+        return "Other test";
     }
 }
